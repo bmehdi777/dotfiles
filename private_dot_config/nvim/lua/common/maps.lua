83fadbfb39eac3eb619b528e0ee7393961b83bcb
Mehdi <mehdi.bentouati76@gmail.com>
2022-11-16 14:47:42 +0100

    Update .config/nvim/lua/common/maps.lua

--- private_dot_config/nvim/lua/common/maps.lua
+++ private_dot_config/nvim/lua/common/maps.lua
@@ -6,15 +6,15 @@ local inoremap = keymap.inoremap
 vim.g.mapleader = ","
 
 -- Navigate through pane
-nnoremap("<C-h>", ":wincmd h<CR>")
-nnoremap("<C-j>", ":wincmd j<CR>")
-nnoremap("<C-k>", ":wincmd k<CR>")
-nnoremap("<C-l>", ":wincmd l<CR>")
+nnoremap("<C-h>", ":wincmd h<CR>", { silent=true })
+nnoremap("<C-j>", ":wincmd j<CR>", { silent=true })
+nnoremap("<C-k>", ":wincmd k<CR>", { silent=true })
+nnoremap("<C-l>", ":wincmd l<CR>", { silent=true })
 
 -- Yank to clipboard
 vnoremap("<leader>y", '"+y')
 nnoremap("<leader>y", '"+y')
 
 -- Tree
-nnoremap("<C-b>", ":silent NvimTreeToggle<CR>", { silent=true })
-inoremap("<C-b>", ":silent NvimTreeToggle<CR>", { silent=true })
+nnoremap("<C-b>", ":NvimTreeToggle<CR>", { silent=true })
+inoremap("<C-b>", ":NvimTreeToggle<CR>", { silent=true })
