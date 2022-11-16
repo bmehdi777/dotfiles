86b945ded8dfee7497efb0d420713c88b9f41f91
Mehdi <mehdi.bentouati76@gmail.com>
2022-11-16 11:53:57 +0100

    Update .local/share/chezmoi/run_once_install-nvim.sh Update install-nvim.sh

--- run_once_install-nvim.sh
+++ run_once_install-nvim.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+if [ ! -d "~/.local/share/nvim/site/pack/packer/" ]; then
+	# Download packer
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+	~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
+	# Install plugin
+	nvim --headless -c "PackerSync"
+fi
