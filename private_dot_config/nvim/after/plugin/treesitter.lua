4e0e46f0d6178e0f8377d15ec6d013a802886ab8
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-01-19 00:54:43 +0100

    Add .config/nvim/after/plugin/treesitter.lua Update .config/nvim/lua/common/packer.lua

---
+++ private_dot_config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,19 @@
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+treesitter.setup {
+  ensure_installed = { "lua", "rust", "toml" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  ident = { enable = true }, 
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
