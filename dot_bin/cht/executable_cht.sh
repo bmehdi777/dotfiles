dbeb26c671cde2a0b2acb28d4ce1f4eb3d090465
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-03-26 04:12:47 +0200

    Update .bin/cht/cht.sh

--- dot_bin/cht/executable_cht.sh
+++ dot_bin/cht/executable_cht.sh
@@ -5,5 +5,9 @@ selected=$(cat ~/.bin/cht/cht.lang ~/.bin/cht/cht.cmd | fzf)
 
 read -p "Search: " query
 
-query=`echo $query | tr ' ' '+'`
-tmux split-window -h bash -c "echo \"cht.sh/$selected/$query\" & curl -s cht.sh/$selected/$query | less -r"
+if [[ -z $query ]]; then
+	tmux split-window -h bash -c "echo \"cht.sh/$selected/$query\" & curl -s cht.sh/$selected | less -r"
+else
+	query=`echo $query | tr ' ' '+'`
+	tmux split-window -h bash -c "echo \"cht.sh/$selected/$query\" & curl -s cht.sh/$selected/$query | less -r"
+fi
