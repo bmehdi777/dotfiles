74f33c0ab4241f6e9d0a6e876f64a8a68ff07592
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2023-11-13 01:31:54 +0100

    Update .config/nvim/after/plugin/telescope.lua

--- private_dot_config/nvim/after/plugin/telescope.lua
+++ private_dot_config/nvim/after/plugin/telescope.lua
@@ -8,6 +8,7 @@ local nnoremap = require("common/keymap").nnoremap
 
 telescope.setup {
 	defaults = {
+		file_ignore_patterns =  { "node_modules" },
 		mappings = {
 			i = {
 				["<C-j>"] = actions.move_selection_next,
