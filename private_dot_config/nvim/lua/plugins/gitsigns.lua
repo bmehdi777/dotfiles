c882dc18dd6bb868c2c9980f784619818bc8c2ce
Bentouati Mehdi <mehdi.bentouati@docaposte.fr>
2024-02-06 08:57:18 +0100

    Update .config/nvim/cheatsheet.txt Update .config/nvim/lua/plugins/gitsigns.lua Update .config/nvim/lua/plugins/prettier.lua

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,7 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	config = function() 
+		local gitsigns = require("gitsigns")
+		gitsigns.setup()
+	end
 }
--- private_dot_config/nvim/lua/plugins/prettier.lua
+++ private_dot_config/nvim/lua/plugins/prettier.lua
@@ -1,6 +1,23 @@
 return {
 	"MunifTanjim/prettier.nvim",
 	config = function()
-
+		local prettier = require("prettier")
+		prettier.setup({
+			bin = 'prettier',
+			filetypes = {
+				"css",
+				"html",
+				"javascript",
+				"javascriptreact",
+				"json",
+				"markdown",
+				"yaml"
+			},
+			cli_options = {
+				semi = true,
+				trailling_coma = "all",
+				single_quote = false,
+			}
+		})
 	end
 }
