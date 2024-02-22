c0d78f23b14146fbf902215ab4430a70e2b0b218
Bentouati Mehdi <mehdi.bentouati76@gmail.com>
2024-02-22 23:28:49 +0100

    Update .config/nvim/cheatsheet.txt Add .config/nvim/lua/core/autocmd.lua

---
+++ private_dot_config/nvim/lua/core/autocmd.lua
@@ -0,0 +1,14 @@
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+	end,
+})
