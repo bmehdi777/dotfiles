620adc8ecbab7de6ceb48945a49156c52ff17f3f
Bentouati Mehdi <mehdi.bentouati@docaposte.fr>
2024-05-22 09:42:05 +0200

    Update .config/polybar/config.ini Update .config/polybar/launch.sh

--- private_dot_config/polybar/executable_launch.sh
+++ private_dot_config/polybar/executable_launch.sh
@@ -9,6 +9,48 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # User configuration
-## Launch bar 1 & 2
-polybar one -c ~/.config/polybar/config.ini &
-polybar two -c ~/.config/polybar/config.ini &
+#
+# detect layout
+SCREENLAYOUT=$(autorandr --detected 2>/dev/null)
+SCREENNAMES=$(autorandr --fingerprint 2>/dev/null | awk '{print $1}')
+
+case $SCREENLAYOUT in
+	"triple_work")
+		triple_work()
+		;;
+	"double_work")
+		double_work()
+		;;
+	"single")
+		single()
+		;;
+	"triple_home")
+		triple_home()
+		;;
+esac
+
+
+triple_work() {
+	export DISPLAY_THREE=$(echo $SCREENNAMES | awk 'NR==3 {print}')
+	export EXTERNAL_MONITOR_1=$(echo $SCREENNAMES | awk 'NR==1 {print}')
+	export EXTERNAL_MONITOR_2=$(echo $SCREENNAMES | awk 'NR==2 {print}')
+
+	export DISPLAY_ONE=$()
+	polybar triple_work_one -c ~/.config/polybar/config.ini &
+	polybar triple_work_two -c ~/.config/polybar/config.ini &
+	polybar triple_work_three -c ~/.config/polybar/config.ini &
+}
+double_work() {
+	polybar double_work_one -c ~/.config/polybar/config.ini &
+	polybar double_work_two -c ~/.config/polybar/config.ini &
+}
+single() {
+	export DISPLAY_ONE=$(echo $SCREENNAMES)
+	polybar single -c ~/.config/polybar/config.ini &
+}
+triple_home() {
+	polybar triple_home_one -c ~/.config/polybar/config.ini &
+	polybar triple_home_two -c ~/.config/polybar/config.ini &
+	polybar triple_home_three -c ~/.config/polybar/config.ini &
+}
+
