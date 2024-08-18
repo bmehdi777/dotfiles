91cc201e217700139777f557648e8588c64d594f
Mehdi <bmehdi>
2024-08-19 00:08:50 +0200

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,11 +1,12 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Argonaut'
---config.color_scheme = 'Tokyo Night'
+--config.color_scheme = 'Argonaut'
+config.color_scheme = 'Tokyo Night'
+--config.color_scheme = 'Ayu Dark (Gogh)'
 config.font = wezterm.font 'JetBrains Mono'
 
 config.enable_tab_bar = false
-config.automatically_reload_config = false
+config.automatically_reload_config = true
 
 return config
