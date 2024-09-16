731fec9a50311bf666d5a979b1d129d0ae64160e
bmehdi777 <mehdi.bentouati76@gmail.com>
2024-09-16 23:13:52 +0200

    Update .config/nvim/lua/core/autocmd.lua

--- private_dot_config/nvim/lua/core/autocmd.lua
+++ private_dot_config/nvim/lua/core/autocmd.lua
@@ -19,3 +19,12 @@ vim.api.nvim_create_user_command('Browse',
 	end,
 	{ nargs = 1 }
 )
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  callback = function(event)
+    local opts = { buffer = event.buf, silent = true }
+    vim.keymap.set('n', '<C-n>', '<cmd>cn | wincmd p<CR>', opts)
+    vim.keymap.set('n', '<C-p>', '<cmd>cN | wincmd p<CR>', opts)
+  end,
+})
