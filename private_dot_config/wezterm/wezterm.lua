e64aacecc7402159762ec4d38f24bae0cb9b79c9
bmehdi777 <mehdi.bentouati76@gmail.com>
2025-01-17 21:05:19 +0100

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -3,10 +3,12 @@ local config = wezterm.config_builder()
 
 --config.color_scheme = 'Argonaut'
 config.color_scheme = 'Tokyo Night'
---config.color_scheme = 'Ayu Dark (Gogh)'
+--config.color_scheme = 'Afterglow'
 config.font = wezterm.font 'JetBrains Mono'
 
 config.enable_tab_bar = false
 config.automatically_reload_config = true
+
+config.font_size = 10.0
 
 return config
