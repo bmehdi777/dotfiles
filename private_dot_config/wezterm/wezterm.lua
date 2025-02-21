5f4f3d3535cae008a81a7963a01319217de849e1
Bentouati Mehdi <mehdi.bentouati@docaposte.fr>
2025-02-21 14:45:31 +0100

    Update .config/wezterm/wezterm.lua

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -5,8 +5,11 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tokyo Night'
 --config.color_scheme = 'Ayu Dark (Gogh)'
 config.font = wezterm.font 'JetBrains Mono'
+-- Remove ligature
+config.harfbuzz_features = { 'calt=0' }
 
 config.enable_tab_bar = false
 config.automatically_reload_config = true
+config.warn_about_missing_glyphs = false
 
 return config
